c1215b04ffbddad172788a8707b4b2e7369e20b7
Praveen <praveen.k@xnara.com>
2025-03-05 22:21:13 +0530

    added array methods

--- arrays.js
+++ arrays.js
@@ -108,5 +108,99 @@ console.log("Splice", arrays)
 // endIndex - The position where you want to end the slice
 console.log(arrays.slice(-3, -1)) // -3..-1
 
+// 8. toString()
+// Syntax:-
+// arrayName.toString()
+console.log(arrays.toString())
+
+// 9. indexOf(element)
+// Syntax:-
+// arrayName.indexOf(element)
+
+const array1 = [10,50,30,40,10,20,"TS", "JS"]
+console.log(array1.indexOf(10, 2)) 
+
+// forEach(function), map(fun), filter(fun), reduce(fun), some(fun), every(fun)
+
+
+// 10. forEach(function)
+// Syntax:-
+// arrayName.forEach(function(element, index, array){})
+
+// element - The current element being processed - 10-0, 50-1, 30-2
+// index - The index of the current element
+// array - The array forEach() was called upon
+
+
+
+array1.forEach(function(element, index){
+    console.log(element)
+})
+console.log("**************")
+for(let i=0; i<array1.length; i++){
+    console.log(array1[i], i)
+}
+
+
+// 11. map(fun) - Create a new array
+// Syntax:-
+// arrayName.map(function(element, index, array){})
+
+// element - The current element being processed - 10-0, 50-1,
+// index(optional) - The index of the current element
+// array (optional) - The array map() was called upon
+
+let array2 = [10, 20, 30, 40, 50, 13]
+
+const mapArray = array2.map(function(element){
+    return element * element
+})
+console.log(mapArray)
+
+// 12. filter(fun) - Create a new array but will return us only those values which passes the function
+// Syntax:-
+// arrayName.filter(function(element, index, array){})
+ 
+// // element - The current element being processed
+// index(optional) - The index of the current element
+// array (optional) - The array filter() was called upon
+
+const filterArray = array2.filter(function(element){
+    return element % 2 ==1
+})
+console.log(filterArray)
+
+// 13. some(fun) - Return us the value as true or false - if at least one element passes the constion true else it will retun us a false
+// Syntax:-
+
+// arrayName.some(function(element, index, array){})
+
+// element - The current element being processed
+// index(optional) - The index of the current element
+// array (optional) - The array some() was called upon
+
+const someArray = array2.some(function(element){
+    return element > 50
+})
+console.log(someArray)
+
+// 14. every(fun) - Return us the value as true or false - if all the elements passes the
+// Syntax:-
+// arrayName.every(function(element, index, array){})
+
+// element - The current element being processed
+// index(optional) - The index of the current element
+// array (optional) - The array every() was called upon
+
+let array3 = [1,2,3,4,5,7]
+
+const everyArray = array3.every(function(element){
+    return element > 1
+    })
+    console.log(everyArray)
+
+
+
+    // 15. reduce(function) - 
 
 
